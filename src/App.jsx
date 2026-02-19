697b2969eafb47c1cc0773964c8324efb35971ed
haru-spec23 <haruto20040823@gmail.com>
2026-02-19 20:09:47 +0900

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main style={{ fontFamily: "system-ui", padding: 20 }}>
+      <h1>金欠or貯金シミュレーター</h1>
+      <p>GitHub Pages表示テスト成功！</p>
+    </main>
+  );
+}
